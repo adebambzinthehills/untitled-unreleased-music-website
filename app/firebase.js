0d609cf663212a0d297be561d56dae779a7977a4
Ayomide Balogun <ayomide11@icloud.com>
2024-01-05 15:05:34 +0000

    import sdks for firebase

--- app/firebase.js
+++ app/firebase.js
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { } from 'firebase/firestore';
+import { } from 'firebase/auth';
+import { } from 'firebase/functions';
+import { } from 'firebase/storage';
+import { } from 'firebase/database';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +25,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export default app;
