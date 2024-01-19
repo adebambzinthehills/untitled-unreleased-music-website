98dbecf1e6f84ce58cad06ec88744c780236c525
Ayomide Balogun <ayomide11@icloud.com>
2024-01-19 16:45:39 +0000

    make cards clickable

--- app/src/components/LibraryCard.jsx
+++ app/src/components/LibraryCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { BsThreeDots } from "react-icons/bs";
+import { FaPlay } from "react-icons/fa";
 
 
 
@@ -9,6 +10,9 @@ function LibraryCard({title, artist, image, type}) {
 
   return (
     <div className='card-container grid-item'>
+      <div className='card-click'>
+        <button className='card-click-button'></button>
+      </div>
       <div className="card-header"> 
         <button>
             <BsThreeDots/>
@@ -25,8 +29,9 @@ function LibraryCard({title, artist, image, type}) {
         </a>
         <p>{type} • {artist} </p>
       </div>
-      <div className='card-play-button'>
+      <div className='card-play-content'>
         {/* absolute position, floating right, border radius 500, z index 2 */}
+        <button className='card-play-button'><span><FaPlay/></span></button>
       </div>
     </div>
   )
