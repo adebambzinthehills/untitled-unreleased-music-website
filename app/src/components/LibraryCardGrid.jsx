8d692ad95fb09009b790ed4f9f0ec8ee0861b677
Ayomide Balogun <ayomide11@icloud.com>
2024-01-19 19:47:49 +0000

    add search bar

--- app/src/components/LibraryCardGrid.jsx
+++ app/src/components/LibraryCardGrid.jsx
@@ -5,14 +5,19 @@ import ctv30 from '../images/ctv301.jpeg'
 import erys from '../images/erys.webp'
 import syre from '../images/syre.jpeg'
 import { FaPlus } from "react-icons/fa";
+import likedsongs from "../images/liked-songs.jpeg";
 
 
 function LibraryCardGrid() {
   return (
     <div className='libraryWrapper'>
-      <div className='search-bar-wrapper'>
-        <div className='search-bar'>
-          <input></input>
+      <div className='search-bar-wrapper container'>
+        <div className='search-bar-item'>
+          <input className='search-bar' placeholder='Search for an song, album or playlist here.'></input>
+        </div>
+        <div className='search-bar-row'>
+          <button>Albums</button>
+          <button>Playlists</button>
         </div>
       </div>
       <div className='grid-card-container'>
@@ -20,6 +25,7 @@ function LibraryCardGrid() {
         <LibraryCard title="CTV3: Cool Tape Vol. 3" artist="Jaden" image={ctv30} type="Album"></LibraryCard>
         <LibraryCard title="ERYS (Deluxe)" artist="Jaden" image={erys} type="Album"></LibraryCard>
         <LibraryCard title="SYRE" artist="Jaden" image={syre} type="Album"></LibraryCard>
+        <LibraryCard title="All Songs" artist="[artistname]" image={likedsongs} type="Playlist"></LibraryCard>
       </div>
       <div className='library-footer'>
         <div className='library-footer-content'>
