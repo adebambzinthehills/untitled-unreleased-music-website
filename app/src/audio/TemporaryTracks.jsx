418bbc1dbfb6b51f0658e00cf628631c328b6cf3
Ayomide Balogun <ayomide11@icloud.com>
2024-02-16 15:44:22 +0000

    breakpoint before i implement multiple refs for progress bar

--- app/src/audio/TemporaryTracks.jsx
+++ app/src/audio/TemporaryTracks.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import seeyoulater from './seeyoulater.mp3'
 import jane from './jane.wav'
+import jane2 from './jane2.mp3'
+import equestrian from './equestrian.mp3'
+import stratfordgirl from './stratfordgirl.mp3'
 
 export const tracks = [
     {
         title: 'i can touch my toes!',
         src: jane,
         author: 'adebambz!',
-        thumbnail: ''
+        thumbnail: '',
+        album: 'do you want to go higher?'
     },
     {
         title: 'see you later! (demo)',
         src: seeyoulater,
         author: 'adebambz!',
-        thumbnail: ''
+        thumbnail: '',
+        album: 'do you want to go higher?'
+    },
+    {
+        title: 'equestrian trail freestyle #2',
+        src: equestrian,
+        author: 'adebambz!',
+        thumbnail: '',
+        album: 'do you want to go higher?'
+    }
+    ,
+    {
+        title: 'jane removed this',
+        src: jane2,
+        author: 'adebambz!',
+        thumbnail: '',
+        album: 'do you want to go higher?'
+    },
+    {
+        title: 'stratford girl',
+        src: stratfordgirl,
+        author: 'nayahlone',
+        thumbnail: '',
+        album: 'stratford girl'
     }
 ]
--- app/src/components/Player.jsx
+++ app/src/components/Player.jsx
@@ -27,7 +27,8 @@ function Player() {
 
     const [isPlaying, setIsPlaying] = useState(false);
     const music = useRef();
-    const [currentTrack, setCurrentTrack] = useState(tracks[1]);
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [currentTrack, setCurrentTrack] = useState(tracks[trackIndex]);
     const [duration, setDuration] = useState(0);
 
     const {playerOn, play, stop, toggle, playerImgSrc, changePlayerImage,
@@ -41,6 +42,8 @@ function Player() {
     const [timeProgress, setTimeProgress] = useState(0);
 
     const animationRef = useRef();
+
+    
 
     useEffect(() => {
 
@@ -123,6 +126,7 @@ function Player() {
     const repeatAnimationFrame = useCallback(() => {
 
         if(playerOn){
+            if(music.current && progressBarRef.current){
             const currentTime = music.current.currentTime;
 
             setTimeProgress(currentTime);
@@ -134,6 +138,7 @@ function Player() {
             );
 
             animationRef.current = requestAnimationFrame(repeatAnimationFrame);
+            }
         }
         
     }, [music, duration, progressBarRef, setTimeProgress]);
@@ -146,7 +151,7 @@ function Player() {
             music.current.pause();
         }
         animationRef.current = requestAnimationFrame(repeatAnimationFrame);
-    }, [isPlaying, music, repeatAnimationFrame])
+    }, [isPlaying, music, repeatAnimationFrame, playerFullscreen])
 
 
     const greenShuffle = shufflePlayer ? 'green-shuffle' : '';
@@ -192,21 +197,108 @@ function Player() {
         }
         return '00:00';
     };
-
     
+    function skipPlay() {
+        setPlayPress(true);
+        setIsPlaying(true);
+    }
+    //confirm state update when track
+    useEffect(() => { console.log('New track index', trackIndex)}, [trackIndex] )
+
+    function handleSkip() {
+        let currentIndex = trackIndex;
+        
+        if(!(repeatCount == 2)){
+            if(repeatCount == 0){
+                if(currentIndex >= (tracks.length - 1) ){
+                    currentIndex = 0
+                    setPlayPress(false);
+                    setIsPlaying(false);    
+                    setTrackIndex((currentIndex));
+                    setCurrentTrack(tracks[currentIndex])
+                    console.log("I'm here too somewhere!")
+                }
+                else{
+                    skipPlay();
+                    setTrackIndex((currentIndex + 1));
+                    setCurrentTrack(tracks[currentIndex + 1])
+                }
+                
+            }
+            else{
+                if(trackIndex >= tracks.length - 1 ){
+                    setTrackIndex(0);
+                    currentIndex = 0
+                    setCurrentTrack(tracks[currentIndex]);
+                }
+                else{
+                    setTrackIndex(currentIndex + 1);
+                    setCurrentTrack(tracks[currentIndex + 1])
+                    skipPlay();
+                }
+            }
+        }
+        else {
+            progressBarRef.current.currentTime = 0;
+            music.current.currentTime = 0;
+            music.current.play();
+        }
+    }
+
+    function handleReload() {
+        let currentIndex = trackIndex;
+        if(!(repeatCount == 2)){
+            if(!(music.current.currentTime <= 3) && currentIndex != 0){
+                if(music.current.currentTime <= 0.5){
+                    if(trackIndex == 0){
+                        currentIndex = tracks.length - 1;
+                        setTrackIndex(currentIndex);
+                        setCurrentTrack(tracks[currentIndex]);
+                    }
+                    else{
+                        setTrackIndex(currentIndex- 1);
+                        setCurrentTrack(tracks[currentIndex - 1])
+                    }
+                }
+                else{
+                    progressBarRef.current.currentTime = 0;
+                    music.current.currentTime = 0;
+                }
+            }
+            else {
+                if(trackIndex == 0){
+                    currentIndex = 0
+                    setTrackIndex(currentIndex);
+                    setCurrentTrack(tracks[currentIndex]);
+                    progressBarRef.current.currentTime = 0;
+                    music.current.currentTime = 0;
+                }
+                else{
+                    setTrackIndex(currentIndex- 1);
+                    setCurrentTrack(tracks[currentIndex - 1])
+                }
+                music.current.play();
+            }
+        }
+        else {
+            progressBarRef.current.currentTime = 0;
+            music.current.currentTime = 0;
+        }
+        setPlayPress(true);
+        setIsPlaying(true);  
+    }
 
 
   return (
 
     <div>
-    <audio src={currentTrack.src} ref={music} onLoadedMetadata={() => onLoadedMetadata()}/>
-      {
+    <audio src={currentTrack.src} ref={music} onLoadedMetadata={() => onLoadedMetadata()} onEnded={() => handleSkip()}/>
       <div className='player-container'>
         <div className='row player-row'>
             <div className='col-3 player-col'>
                 <div className='left-information-wrapper'>
                     <div className='player-image-wrapper'>
-                        <button className='player-image-button'><img className='player-image' src={playerImgSrc}></img></button>
+                        <button className='player-image-button'><img className='player-image' src={(currentTrack.thumbnail == '' || currentTrack.thumbnail == null ) ? playerImgSrc : currentTrack.thumbnail }></img></button>
                     </div>
                     <div className='player-song-information-wrapper'>
                         <div>
@@ -226,13 +318,13 @@ function Player() {
                             {shufflePlayer && <span className='player-shuffle-button-dot'><GoDotFill/></span>}
                         </div>
                         <div className='player-button-wrapper'>
-                            <button className='player-rewind-button'><span><IoIosSkipBackward/></span></button>
+                            <button className='player-rewind-button' onClick={() => handleReload()}><span><IoIosSkipBackward/></span></button>
                         </div>
                         <div className='player-button-wrapper'>
                             <button className='player-play-button' onClick={() => handlePlayPress()}><span>{playPress ? <IoMdPause className='player-pause-button'/> : <FaPlay/>}</span></button>
                         </div>
                         <div className='player-button-wrapper'>
-                            <button className='player-skip-button'><span><IoIosSkipBackward/></span></button>
+                            <button className='player-skip-button' onClick={() => handleSkip()}><span><IoIosSkipBackward/></span></button>
                         </div>
                         <div className='player-button-wrapper'>
                             <button className={'player-repeat-button ' + greenRepeat} onClick={() => handleRepeat()}><span>{repeatCount <= 1? <LuRepeat/> : <LuRepeat1/>}</span></button>
@@ -264,21 +356,20 @@ function Player() {
                 </div>
             </div>
         </div>
-      </div>}
-      {
+      </div>
       <div className='miniplayer-container' style={playerColour}>
         <div className='row miniplayer-row'>
             <div className='col-10 player-col '>
                 <div className='miniplayer-left-information-wrapper'>
                     <div className='miniplayer-image-wrapper'>
-                        <button className='miniplayer-image-button'><img className='miniplayer-image' src={playerImgSrc}></img></button>
+                        <button className='miniplayer-image-button'><img className='miniplayer-image' src={(currentTrack.thumbnail == '' || currentTrack.thumbnail == null ) ? playerImgSrc : currentTrack.thumbnail }></img></button>
                     </div>
                     <div className='miniplayer-song-information-wrapper'>
                         <div>
-                            <span className='player-song-name'><a>Santa Barbara</a></span>
+                            <span className='player-song-name'><a>{currentTrack.title}</a></span>
                         </div>
                         <div className='span-information-wrap'>
-                            <span className='player-artist-name'>Jaden</span>
+                            <span className='player-artist-name'>{currentTrack.author}</span>
                         </div>
                     </div>
                 </div>
@@ -292,21 +383,21 @@ function Player() {
                 </div>
             </div>
         </div>
-      </div>}
-      { playerFullscreen &&
-        <div className='fullscreen-player'>
+      </div>
+        <div className='fullscreen-player' style={playerFullscreen? 
+            {display: 'block'} : {display: 'none'}}>
         <div className='fullscreen-player-header row'> 
-            <div className='col-4 fullscreen-player-close'>
+            <div className='col-2 fullscreen-player-close'>
                 <button className="fullscreen-player-close-button" onClick={() => {setPlayerFullscreen(false); disableFullscreenPlayer();}}>
                     <span><IoChevronBack/></span>
                 </button>
             </div>
-            <div className='col-4 fullscreen-player-content-title-wrapper'>
+            <div className='col-8 fullscreen-player-content-title-wrapper'>
                 <div className='fullscreen-player-content-title'>
-                    <span>#STILLSWAGGIN</span>
+                    <span>{currentTrack.album}</span>
                 </div>
             </div>
-            <div className='col-4 fullscreen-player-three-dots'>
+            <div className='col-2 fullscreen-player-three-dots'>
                 <div className='fullscreen-player-three-dots-wrapper'>
                     <button className='fullscreen-player-three-dots-button'>
                         <span><BsThreeDots/></span>
@@ -317,31 +408,33 @@ function Player() {
         <div className='fullscreen-player-image-section'>
             <div className='fullscreen-player-image-wrapper'>
                 <div className='fullscreen-player-image-square'>
-                    <img className='fullscreen-player-image' src={playerImgSrc}/>
+                    <img className='fullscreen-player-image' src={(currentTrack.thumbnail == '' || currentTrack.thumbnail == null ) ? playerImgSrc : currentTrack.thumbnail }/>
                 </div>
             </div> 
         </div>
         <div className='fullscreen-player-information-section'>
             <div className='fullscreen-player-information-wrapper'>
                 <div className='fullscreen-player-song-title'>
-                    <span><h1>Again (feat. SYRE)</h1></span>
+                    <span><h1>{currentTrack.title}</h1></span>
                 </div>
                 <div className='fullscreen-player-artist-name'>
-                    <span>Jaden</span>
+                    <span>{currentTrack.author}</span>
                 </div>
             </div>
         </div>
         <div className='fullscreen-player-rest-section'>
             <div className='fullscreen-player-controls-wrapper'>
                 <div className='player-scrollbar-wrapper'>
-                    <div className='fullscreen-scrollbar'>
-                        <div><span className='player-time-left'>0:00</span></div>
-                        <div className='fullscreen-scrollbar-overlay'>
-                        <div className='player-scrollbar-dot'></div>
+                    <div className='player-time-wrapper'><span className='player-time-left'>{formatTime(timeProgress)}</span></div>
+                    <div className='player-scrollbar'>
+                        {/* <div className='player-scrollbar-grey'>
+                            <div className='player-scrollbar-overlay'>
+                                <div className='player-scrollbar-dot'></div>
+                            </div>
+                        </div> */}
+                        <input className="player-scrollbar-input" type="range" onChange={() => handleProgress()} ref={progressBarRef}></input>
                     </div>
-                    <div className='player-scrollbar-grey'></div>
-                        <div><span className='player-time-right'>2:22</span></div>
-                    </div>
+                    <div className='player-time-wrapper'><span className='player-time-right'>{formatTime(duration)}</span></div>
                 </div>
                 <div className='fullscreen-buttons-wrapper row'>
                     <div className='col-2-4 player-buttons-section-col'>
@@ -352,7 +445,7 @@ function Player() {
                     </div>
                     <div className='col-2-4'>
                         <div className='fullscreen-button-wrapper media-flex-middle'>
-                            <button className='player-rewind-button'><span><IoIosSkipBackward/></span></button>
+                            <button className='player-rewind-button' onClick={() => handleReload()}><span><IoIosSkipBackward/></span></button>
                         </div>
                     </div>
                     <div className='col-2-4 player-buttons-section-col flex-middle'>
@@ -362,7 +455,7 @@ function Player() {
                     </div>
                     <div className='col-2-4 player-buttons-section-col'>
                         <div className='fullscreen-button-wrapper media-flex-middle'>
-                            <button className='player-skip-button'><span><IoIosSkipBackward/></span></button>
+                            <button className='player-skip-button' onClick={() => handleSkip()}><span><IoIosSkipBackward/></span></button>
                         </div>
                     </div>
                     <div className='col-2-4'>
@@ -383,10 +476,8 @@ function Player() {
                     </div>
                 </div>
             </div>
+        </div>   
         </div>
-            
-        </div>
-      }
       
     </div>
   )
