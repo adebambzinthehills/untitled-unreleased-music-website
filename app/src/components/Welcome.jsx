6c7f3f96554a950ef416d1d7eb60cce0296a841a
Ayomide Balogun <ayomide11@icloud.com>
2024-03-25 17:15:44 +0000

    add guidance to welcome page and css changes

--- app/src/components/Welcome.jsx
+++ app/src/components/Welcome.jsx
@@ -17,10 +17,14 @@ function Login({loggedIn, setLoggedIn}){
           <p>This website is built for artists to simulate what 
             their unreleased music will look and sound like on the platform way before it goes live,
             helping you to make more creative decisions, and focus on the experience of the music.</p>
-          <button className='loginBtn btn btn-success' onClick={() => navigate("/login")} > Let's start your Preview experience.</button>
+          <p style={{fontSize:'0.9rem', fontStyle:'italic'}}>Here you will be able to customise and change the information associated with a project 
+            (album cover, title, release date and label/publishing information, tracklist & track information) as well as profile information and images. 
+            You will not be able to change the actual design features, such as the library, project and account pages and player design as these are meant to simulate Spotify's
+            interface.</p>
           <p className='small'>This is a solo student developer project by Ayomide Balogun as part 
           of his third year project. </p>
-          <p className='small' style={{fontSize:'0.6rem'}}>This project is still HEAVILY under development, so there will be a few bugs!</p>
+          <p className='small' style={{fontSize:'0.6rem'}}>This project is still under development, so there will be a few bugs!</p>
+          <button className='loginBtn btn btn-success' onClick={() => navigate("/login")} > Let's start your Preview experience.</button>
         </div>
       </div>
     )
