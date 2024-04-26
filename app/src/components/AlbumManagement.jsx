9e8422018dbee4c3e622db99d91948cd53eefea3
Ayomide Balogun <ayomide11@icloud.com>
2024-04-26 12:05:21 +0100

    add required to release date

--- app/src/components/AlbumManagement.jsx
+++ app/src/components/AlbumManagement.jsx
@@ -839,7 +839,7 @@ function AlbumManagement({clickOff, edit, mode, setMode, cards, setCards, setAlb
                             <input className='album-entry' ref={labelText} placeholder='Who is the publisher/label?'  data-bs-toggle="tooltip" data-bs-placement="top" title="This will be displayed at the bottom of the project page, below the tracklist."></input>
                         </div>
                         <div className='album-manager-field-wrapper'>
-                            <div className='label-wrapper'><label htmlFor="">Release Date</label></div>
+                            <div className='label-wrapper'><label htmlFor="">Release Date (Required)</label></div>
                             <input className='album-entry' ref={releaseDate} type='date' placeholder="What's your project release date?" data-bs-toggle="tooltip" data-bs-placement="top" title="This will be displayed next to the project title, and again at the bottom of project page, much like Spotify's interface."></input>
                         </div>
                         <div className='album-manager-field-wrapper'>
